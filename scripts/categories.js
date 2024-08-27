80b120424f44f9e5a62eadb770eb3f8599c26210
katkay <kelvinkaykatoya@gmail.com>
2024-08-27 10:07:29 +0100

    cleaned out some repeated code in modules and made the back to top button in the index page work

--- scripts/categories.js
+++ scripts/categories.js
@@ -31,3 +31,18 @@ categories.forEach((categories) => {
 })
 
 document.querySelector('.js-categories-grid').innerHTML = categoriesHTML;
+
+const backToTop = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        backToTop.style.display = 'block';
+    } else {
+        backToTop.style.display = 'none'
+    }
+});
+
+backToTop.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
